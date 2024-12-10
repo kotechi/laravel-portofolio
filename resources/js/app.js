cb2b0feec62fbb6b91e0f30e2537611ca4ff351a
kotechi <aditiya53op@gmail.com>
2024-12-11 06:12:30 +0700

    Initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,5 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
-import ApexCharts from 'apexcharts';
 import Alpine from 'alpinejs';
-import LarapexChart from 'larapex-charts';
-
-window.LarapexChart = LarapexChart;
-window.ApexCharts = ApexCharts;
 window.Alpine = Alpine;
 Alpine.start();
